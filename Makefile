7c2906d979ab174c5c8b4231e9e98f801822f03a
Elieren <elieren@dotnet.net>
2025-08-18 11:23:25 +0700

    Add switch build mode

--- Makefile
+++ Makefile
@@ -1,66 +1,54 @@
-###############################
-# Makefile (без GRUB)        #
-# Собирает артефакты в build #
-###############################
-
-# Инструменты
 CC      := gcc
 LD      := ld
 AS      := nasm
 QEMU    := qemu-system-i386
 
-# Флаги
+# Базовые флаги
 CFLAGS   := -m32
 LDFLAGS  := -m elf_i386 -T link.ld
 ASMFLAGS := -f elf32
 
-# Список исходников (можно дополнять)
+# Источники
 SRCS_AS := kernel.asm idt_load.asm interrupt/isr32.asm interrupt/isr33.asm interrupt/isr_stubs.asm interrupt/isr80.asm
 SRCS_C  := kernel.c vga/vga.c keyboard/keyboard.c keyboard/portio.c time/timer.c idt.c pic.c syscall/syscall.c time/clock/clock.c time/clock/rtc.c malloc/malloc.c libc/string.c libc/stack_protector.c
 
-# Объекты в папке build/, сохраняем структуру путей
+# Объекты
 ASM_OBJS := $(patsubst %.asm,build/%.asm.o,$(SRCS_AS))
 C_OBJS   := $(patsubst %.c,build/%.c.o,$(SRCS_C))
 OBJECTS  := $(ASM_OBJS) $(C_OBJS)
 
-# Итоговый бинарник в build
 BUILD_KERNEL := build/kernel
-
-# Дополнительные опции для QEMU (можешь переопределить: make run QEMU_OPTS="-m 256 -s -S")
 QEMU_OPTS ?=
 
 .PHONY: all clean builddir run debug
 
-# По умолчанию: собрать ядро
+# --- build ---
 all: builddir $(BUILD_KERNEL)
 
-# Создать корневую папку сборки (если ещё нет)
 builddir:
 	@mkdir -p build
 
-# Правило для ассемблирования .asm → build/%.asm.o (с созданием нужной директории)
 build/%.asm.o: %.asm
 	@mkdir -p $(dir $@)
 	$(AS) $(ASMFLAGS) $< -o $@
 
-# Правило для компиляции .c → build/%.c.o (с созданием нужной директории)
 build/%.c.o: %.c
 	@mkdir -p $(dir $@)
-	$(CC) $(CFLAGS) -c $< -o $@
+	$(CC) $(CFLAGS) $(EXTRA_CFLAGS) -c $< -o $@
 
-# Линковка всех object-файлов в единый бинарник (в build/)
 $(BUILD_KERNEL): $(OBJECTS) link.ld
 	@mkdir -p $(dir $@)
 	$(LD) $(LDFLAGS) -o $@ $(OBJECTS)
 
-# Запустить QEMU с build/kernel
-run: $(BUILD_KERNEL)
-	$(QEMU) -kernel $(BUILD_KERNEL) $(QEMU_OPTS)
-
-# Запустить QEMU с сериалом для отладки (использует build/kernel)
-debug: $(BUILD_KERNEL)
+# --- debug ---
+debug: EXTRA_CFLAGS=-DDEBUG
+debug: all
 	$(QEMU) -kernel $(BUILD_KERNEL) -serial stdio $(QEMU_OPTS)
 
-# Удалить всё из build/
+# --- run ---
+run: all
+	$(QEMU) -kernel $(BUILD_KERNEL) $(QEMU_OPTS)
+
+# --- clean ---
 clean:
 	rm -rf build
--- kernel.c
+++ kernel.c
@@ -13,53 +13,28 @@
 #include "malloc/malloc.h"
 #include "libc/string.h"
 
+/* глобальные переменные */
 uint32_t input_len = 0;
 
 /* символы из link.ld */
 extern char _heap_start;
 extern char _heap_end;
 
-void kmain(void)
+/*-------------------------------------------------------------
+    Debug-функции: полностью исключаются из release сборки
+-------------------------------------------------------------*/
+#ifdef DEBUG
+static void debug_run_tests(void)
 {
-    idt_install();
-    init_system_clock();
-    init_timer(1);
-    outb(0x21, 0xFC);
-
-    /* вычисляем размер кучи по линкер-символам */
-    size_t heap_size = (size_t)((uintptr_t)&_heap_end - (uintptr_t)&_heap_start);
-
-    malloc_init(&_heap_start, heap_size);
-
-    asm volatile("sti");
-
-    // const char *msg = "Hello, World!";
-
-    // clean_screen();
-
-    // sys_print_str(msg, 78, 11, WHITE, BLACK);
-
-    clean_screen();
-
-    update_hardware_cursor();
-
-    for (uint32_t i = 0; prompt[i]; ++i)
-    {
-        print_char_long(prompt[i], WHITE, BLACK);
-    }
-
     sys_print_char('X', 5, 10, WHITE, RED);
 
     const char *secs = sys_get_seconds_str();
-
     sys_print_str(secs, 0, 20, WHITE, RED);
 
     /* Пример: выделить 2 MiB через syscall */
     void *p = sys_malloc(2 * 1024 * 1024);
-
     if (p)
     {
-        /* записать строку в выделенную память */
         char *s = (char *)p;
         strcpy(s, "Hello from kernel heap!");
         sys_print_str(s, 50, 15, WHITE, RED);
@@ -76,8 +51,44 @@ void kmain(void)
         sys_free(p);
     }
 
-    // print_kmalloc_stats();
+    print_kmalloc_stats();
+}
+#endif // DEBUG
 
+/*-------------------------------------------------------------
+    Основная функция ядра
+-------------------------------------------------------------*/
+void kmain(void)
+{
+    /* Инициализация прерываний и таймера */
+    idt_install();
+    init_system_clock();
+    init_timer(1);
+    outb(0x21, 0xFC); // маска прерываний
+
+    /* Вычисляем размер кучи по линкер-символам */
+    size_t heap_size = (size_t)((uintptr_t)&_heap_end - (uintptr_t)&_heap_start);
+    malloc_init(&_heap_start, heap_size);
+
+    /* Разрешаем прерывания */
+    asm volatile("sti");
+
+    /* Очистка экрана и курсор */
+    clean_screen();
+    update_hardware_cursor();
+
+    /* Отображение приветственного текста (prompt) */
+    for (uint32_t i = 0; prompt[i]; ++i)
+    {
+        print_char_long(prompt[i], WHITE, BLACK);
+    }
+
+    /* Запуск debug-фич, если включено */
+#ifdef DEBUG
+    debug_run_tests();
+#endif
+
+    /* Основной бесконечный цикл ядра */
     for (;;)
     {
         asm volatile("hlt");
